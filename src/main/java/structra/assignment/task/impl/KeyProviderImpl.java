2917dfc383deaecc3b00c671a6de2c384144422b
jonahKps <j.kapski24@gmail.com>
2024-12-14 01:20:25 +0100

    added KeyProviderImpl class

---
+++ src/main/java/structra/assignment/task/impl/KeyProviderImpl.java
@@ -0,0 +1,10 @@
+package structra.assignment.task.impl;
+
+import structra.assignment.framework.llm.KeyProvider;
+
+public class KeyProviderImpl implements KeyProvider {
+    @Override
+    public String getApiKey() {
+        return "structra-1343abnc-dGhpcyBpcyBub3Qgb3VyIGFwaSBrZXksIG5pY2UgdHJ5IHRobyA6KQ==\n";
+    }
+}
